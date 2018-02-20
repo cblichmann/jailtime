70970e2beff6fe4094c1154d11657d94746c2ed9
Christian Blichmann <mail@blichmann.eu>
2018-02-20 21:17:48 +0100

    Add basic test for parsing of dynamic loader configs.
    Supports parsing the ld.so.conf format that GNU/Linux ldconfig uses.
    
    Signed-off-by: Christian Blichmann <mail@blichmann.eu>

---
+++ loader/ldconfig_test.go
@@ -0,0 +1,63 @@
+/*
+ * jailtime version 0.6
+ * Copyright (c)2015-2018 Christian Blichmann
+ *
+ * Dynamic loader configuration parsing tests
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package loader // import "blichmann.eu/code/jailtime/loader"
+
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
+func TestParseLdconfig(t *testing.T) {
+	if err := os.Chdir("testdata"); err != nil {
+		t.Error(err)
+	}
+	have := make(map[string]bool)
+	paths := make([]string, 0, 0)
+	for _, v := range ParseLdConfig("ld.so.conf") {
+		t.Log(v)
+		if !have[v] {
+			paths = append(paths, v)
+			have[v] = true
+		}
+	}
+	expected := []string{
+		"/usr/local/lib/i386-linux-gnu",
+		"/lib/i386-linux-gnu",
+		"/usr/lib/i386-linux-gnu",
+		"/usr/local/lib/i686-linux-gnu",
+		"/lib/i686-linux-gnu",
+		"/usr/lib/i686-linux-gnu",
+		"/usr/local/lib/x86_64-linux-gnu",
+		"/lib/x86_64-linux-gnu",
+		"/usr/lib/x86_64-linux-gnu",
+	}
+	if !reflect.DeepEqual(paths, expected) {
+		t.Errorf("expected %s, actual %s", expected, paths)
+	}
+}
